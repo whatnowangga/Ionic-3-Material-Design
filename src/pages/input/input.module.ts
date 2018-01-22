e5e538fe66d8f1431f16f82494230aae35c79e3c
Angga Andrean <anggaand123@gmail.com>
2018-01-22 08:33:57 +0700

    added input page

---
+++ src/pages/input/input.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { InputPage } from './input';
+
+@NgModule({
+  declarations: [
+    InputPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(InputPage),
+  ],
+})
+export class InputPageModule {}
